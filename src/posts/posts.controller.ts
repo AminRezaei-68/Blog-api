48da777d56e42dac282955d26703a7cb82b20d41
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-04 22:00:16 +0330

    add post decorator to controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { PostDocument } from './entities/post.entity';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postService: PostsService) {}
+
+  @Post()
+  async create(@Body() createPostDto: CreatePostDto): Promise<PostDocument> {
+    return this.postService.createPost(createPostDto);
+  }
+}
