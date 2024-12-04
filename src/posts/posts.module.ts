b4339c3ad525fa4ef796b3b23896e62838767fed
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-04 14:09:00 +0330

    posts.module configuration

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,4 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, Post } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostSchema } from './schema/post.schema';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+  ],
+  controllers: [PostsController],
+  providers: [PostsService],
+})
 export class PostsModule {}
