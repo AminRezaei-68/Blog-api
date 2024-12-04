d1df9b3559bbced1da4cfda10998ed2330f7d141
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-04 22:09:15 +0330

    change directory of post entity

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument } from './entities/post.entity';
+import { Model } from 'mongoose';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectModel(Post.name) private postModule: Model<PostDocument>,
+  ) {}
+
+  async createPost(createPostDto: CreatePostDto): Promise<PostDocument> {
+    const { title, content, author } = createPostDto;
+    const newPost = new this.postModule({ title, content, author });
+    return newPost.save();
+  }
+}
