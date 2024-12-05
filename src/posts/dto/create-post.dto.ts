250b139f72c7ab9f6a9fd1d3ba9239238477a632
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-05 19:50:53 +0330

    upgrade create post dto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,22 @@
-import { IsString, IsNotEmpty } from '@nestjs/class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  MaxLength,
+  MinLength,
+} from '@nestjs/class-validator';
 
 export class CreatePostDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(100, { message: 'Title must not exceed 100 characters.' })
   title: string;
 
   @IsString()
+  @MinLength(10, { message: 'Content must be at least 10 characters long.' })
   content: string;
 
   @IsString()
   @IsNotEmpty()
+  @MaxLength(50, { message: 'Author name must not exceed 50 characters.' })
   author: string;
 }
