8f5033a4193405ad2a693f955c15a26e797577f2
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-05 13:38:03 +0330

    remove console log

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -22,7 +22,6 @@ export class PostsService {
 
   async findOne(id: string): Promise<PostEntity> {
     const post = await this.postModule.findById(id);
-    console.log(post);
     if (!post) {
       throw new NotFoundException(`The post with id ${id} not found.`);
     }
