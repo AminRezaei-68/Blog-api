8a2a5d53de2f885018b7dd0da33998d34d22c5b2
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-08 13:30:15 +0330

    create user schema

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: false })
+  isAdmin: boolean;
+}
+
+export class userEntity = SchemaFactory.createForClass(User);
