41888dc39be4e5d2d61e3d8f8620fe6373c6fc9e
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-08 18:26:01 +0330

    upgrade user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, userEntity } from './entities/user.entity';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: userEntity }]),
+  ],
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
+})
 export class UsersModule {}
