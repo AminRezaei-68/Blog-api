ec5b19a7b3b60cea43518e65004bf0a9c44f4a20
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-08 18:21:04 +0330

    create roles guard module

---
+++ src/common/guards/roles/roles.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtAuthGuard } from '../jwt-auth/jwt-auth.guard';
+
+@Injectable()
+export class RolesGuard extends JwtAuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<string[]>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return requiredRoles.some((role) => user.role === role);
+  }
+}
