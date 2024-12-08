c4c37bfbb3a9913de7350a1085379c580fa4d73a
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-08 15:26:12 +0330

    create auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { SignupDto } from 'src/common/dto/signup.dto';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from 'src/common/dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userservice: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signup(signupDto: SignupDto): Promise<{ message: string }> {
+    const existingUser = await this.userservice.findByUsernameOrEmail(
+      signupDto.username,
+    );
+    if (existingUser) {
+      throw new UnauthorizedException('Username or email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(signupDto.password, 10);
+    await this.userservice.create({
+      ...signupDto,
+      password: hashedPassword,
+    });
+
+    return { message: 'User registered successfully' };
+  }
+
+  async login(loginDto: LoginDto): Promise<{ message: string; token: string }> {
+    const user = await this.userservice.findByUsernameOrEmail(
+      loginDto.usernameOrEmail,
+    );
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Incorrect password');
+    }
+
+    const payload = { sub: user.id, username: user.username };
+    const token = this.jwtService.sign(payload);
+
+    return { message: 'Login successful', token };
+  }
+}
