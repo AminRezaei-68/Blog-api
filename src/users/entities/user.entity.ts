7286fa2bfc48cb8364bcebc6857e7cd1f130eec5
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-09 16:55:28 +0330

    add isActive variable to user entity To determine the user status, whether it is active or not

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -13,6 +13,9 @@ export class User extends Document {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop({ default: true })
+  isActive: boolean;
 }
 
 export type UserDocument = User & Document;
