7956fe0cc8a20cb9eeb1d898373648dd8f05d25d
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-10 21:32:23 +0330

    use forwardRef method to solve the undefined in Circular Dependency at posts module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostSchema, PostEntity } from './entities/post.entity';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: PostEntity.name, schema: PostSchema }]),
+    forwardRef(() => UsersModule),
   ],
   controllers: [PostsController],
   providers: [PostsService],
