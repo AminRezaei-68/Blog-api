54ef715149f2c2e56fa96279d00e124f9e6ba367
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-10 21:31:10 +0330

    import posts module into users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, userEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: userEntity }]),
+    PostsModule,
   ],
   providers: [UsersService],
   controllers: [UsersController],
