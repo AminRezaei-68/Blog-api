968ec9dc2593c95db530aed2a23436e1fbf355f0
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-10 09:47:06 +0330

    import userDocument to users service and use it in constructor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from './entities/user.entity';
+import { User, UserDocument } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
@@ -18,7 +18,7 @@ type UserResponse = Partial<User> & { id: string };
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectModel(User.name) private readonly userModel: Model<User>,
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
   ) {}
 
   private async hashPassword(password: string): Promise<string> {
