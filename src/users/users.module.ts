261107d6ef9dde0aee17980ae7c0fc4b1ea6bb09
AminRezaei-68 <rezaei.amin1268@gmail.com>
2025-01-09 16:20:28 +0330

    change the paths of files

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, userEntity } from './entities/user.entity';
+import { User, userSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PostsModule } from 'src/posts/posts.module';
+import { PostsModule } from '../posts/posts.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: userEntity }]),
+    MongooseModule.forFeature([{ name: User.name, schema: userSchema }]),
     PostsModule,
   ],
   providers: [UsersService],
