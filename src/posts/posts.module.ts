69cd98350e3c73bb51941f01b8ab13f3e7635afe
AminRezaei-68 <rezaei.amin1268@gmail.com>
2025-01-09 16:14:24 +0330

    Rename the post name to postSch

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,13 +1,13 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { forwardRef, Module, Post } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PostSchema, PostEntity } from './entities/post.entity';
+import { PostSchema, PostSch } from './schemas/post.schema';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: PostEntity.name, schema: PostSchema }]),
+    MongooseModule.forFeature([{ name: PostSch.name, schema: PostSchema }]),
     forwardRef(() => UsersModule),
   ],
   controllers: [PostsController],
