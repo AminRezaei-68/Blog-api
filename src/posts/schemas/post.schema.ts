f40530e1bd75c432d69c7e7b72e59d480386cb13
AminRezaei-68 <rezaei.amin1268@gmail.com>
2025-01-09 15:47:51 +0330

    Rename the class of post to postSch

--- src/posts/schemas/post.schema.ts
+++ src/posts/schemas/post.schema.ts
@@ -1,15 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-
 @Schema({ timestamps: true })
 export class PostSch {
   @Prop({ required: true })
   title: string;
-  
+
   @Prop()
   content: string;
-  
+
   @Prop({ required: true })
   author: string;
 }
