8eda2a92575c20e3e424e670bc6a30b43086b298
AminRezaei-68 <rezaei.amin1268@gmail.com>
2025-01-09 16:09:54 +0330

    Code modification in last line

--- src/common/guards/roles/roles.guard.ts
+++ src/common/guards/roles/roles.guard.ts
@@ -22,6 +22,7 @@ export class RolesGuard extends JwtAuthGuard implements CanActivate {
     }
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    return requiredRoles.some((role) => user.role === role);
+
+    return requiredRoles.some((role) => user.role.includes(role));
   }
 }
