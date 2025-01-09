7ad39a1055b6716f59a19d8269e17818ee0cea38
AminRezaei-68 <rezaei.amin1268@gmail.com>
2025-01-09 15:54:59 +0330

    modified return object jwt.strategy.ts

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -14,6 +14,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      isActive: payload.userStatus,
+      name: payload.name,
+      role: payload.role,
+    };
   }
 }
